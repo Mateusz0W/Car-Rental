7871492560d38653813f2766c3a8b681cef9f170
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2024-12-23 19:04:33 +0100

    add Main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(final String[] args) {
+ 
+        
+        ClientApp.launch(ClientApp.class,args);
+        
+    }
+}
