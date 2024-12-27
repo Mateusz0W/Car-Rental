e83e84cc232ff731f63053211b953e73f53f8b8d
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2024-12-27 13:34:57 +0100

    add another constructors

--- src/Car.java
+++ src/Car.java
@@ -1,14 +1,23 @@
 public class Car implements Entity {
-    private String brand;
-    private String model;
-    private String category;
-    private int year;
-    private String registration_number;
-    private int mileage;
-    private String status;
-    private int daily_fee;
+    public String brand;
+    public String model;
+    public String category;
+    public int year;
+    public String registration_number;
+    public int mileage;
+    public String status;
+    public double daily_fee;
 
-    Car(String brand,String model,String category,int year,String registration_number,int mileage,String status,int daily_fee){
+    Car(String brand,String model,String category,int year,String registration_number,int mileage,double daily_fee){
+        this.brand=brand;
+        this.model=model;
+        this.category=category;
+        this.year=year;
+        this.registration_number=registration_number;
+        this.mileage=mileage;
+        this.daily_fee=daily_fee;
+    }
+    Car(String brand,String model,String category,int year,String registration_number,int mileage,String status,double daily_fee){
         this.brand=brand;
         this.model=model;
         this.category=category;
@@ -18,14 +27,18 @@ public class Car implements Entity {
         this.status=status;
         this.daily_fee=daily_fee;
     }
-    public String getColumns(){
+    Car(){}
+    public String insertColumns(){
+        return "marka, model, kategoria, rok, numer_rejestracyjny, przebieg, dzienna_oplata";
+    }
+    public String readColumns(){
         return "marka, model, kategoria, rok, numer_rejestracyjny, przebieg, status, dzienna_oplata";
     }
     public String getTable(){
         return "projekt.samochod";
     }
     public Object[] getData(){
-        Object[] data={brand,model,category,year,registration_number,mileage,status,daily_fee};
+        Object[] data={brand,model,category,year,registration_number,mileage,daily_fee};
         return data;
     }
 
