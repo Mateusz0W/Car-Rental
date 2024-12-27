0ba390d0116255cf3f0cca06ee42b18bc20d9c38
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2024-12-27 13:30:59 +0100

    update

--- src/Client.java
+++ src/Client.java
@@ -1,17 +1,20 @@
-public class Clinet implements Entity {
+public class Client implements Entity {
     public String name;
     public String surname;
     public String phone_number;
     public String email;
 
-    Clinet(String name,String surname,String phone_number,String email){
+    Client(String name,String surname,String phone_number,String email){
         this.name=name;
         this.surname=surname;
         this.phone_number=phone_number;
         this.email=email;
     }
-    public String getColumns(){
+    public String insertColumns(){
         return "imie, nazwisko, numer_telefonu, email";
+    }
+    public String readColumns(){
+        return insertColumns();
     }
     public String getTable(){
         return "projekt.klient";
@@ -20,5 +23,5 @@ public class Clinet implements Entity {
         Object[] data={name,surname,phone_number,email};
         return data;
     }
-    
+
 }
