9e64540cadfeae04b374ebce2e3af084151f44cb
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2024-12-30 10:55:14 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class Main {
     public static void main(final String[] args) {
         ClientApp.launch(ClientApp.class,args);
-        
     }
 }
